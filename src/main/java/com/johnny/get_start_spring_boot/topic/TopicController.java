e9aa756b8ff1785780c340f168914c32342c1828
jzhang <jzhang@xogrp.com>
2018-04-11 15:34:14 +0800

    Add Topic Entity

--- src/main/java/com/johnny/get_start_spring_boot/topic/TopicController.java
+++ src/main/java/com/johnny/get_start_spring_boot/topic/TopicController.java
@@ -1,5 +1,8 @@
 package com.johnny.get_start_spring_boot.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
   
   @RequestMapping("/topics")
-  public String getAllTopics(){
-    return "Here are all topics";
+  public List<Topic> getAllTopics(){
+    return Arrays.asList(
+      new Topic("Ruby", "ruby-name", "ruby-description"),
+      new Topic("Java", "java-name", "java-description"),
+      new Topic("CSharp", "csharp-name", "csharp-description"),
+      new Topic("Javascript", "javascript-name", "javascript-description"),
+      new Topic("React", "react-name", "react-description")
+    );
   }
 }
