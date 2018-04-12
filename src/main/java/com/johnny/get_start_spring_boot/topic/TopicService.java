32f344979d9c45c4285a4f6c74ccaf143363518e
jzhang <jzhang@xogrp.com>
2018-04-12 11:27:35 +0800

    Creating a new resource using POST

--- src/main/java/com/johnny/get_start_spring_boot/topic/TopicService.java
+++ src/main/java/com/johnny/get_start_spring_boot/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.johnny.get_start_spring_boot.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,13 +9,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-  private List<Topic> topics = Arrays.asList(
+  private List<Topic> topics = new ArrayList<>(Arrays.asList(
     new Topic("Ruby", "ruby-name", "ruby-description"),
     new Topic("Java", "java-name", "java-description"),
     new Topic("CSharp", "csharp-name", "csharp-description"),
     new Topic("Javascript", "javascript-name", "javascript-description"),
     new Topic("React", "react-name", "react-description")
-  );
+  ));
 
   public List<Topic> getAllTopics(){
     return this.topics;
@@ -22,5 +23,9 @@ public class TopicService {
 
   public Topic getTopic(String id) {
     return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+  }
+
+  public void addTopic(Topic topic) {
+    this.topics.add(topic);
   }
 }
